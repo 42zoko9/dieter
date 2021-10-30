7c93a16abcd10b53a37ddfb113328cd8bc0c5e48
42zoko9 <hexproof1988@gmail.com>
2021-10-30 12:59:10 +0900

    Add: GCP関連の処理を記述するスクリプト追加

---
+++ src/gcp.py
@@ -0,0 +1,23 @@
+from google.cloud import storage
+
+def store_gcs(
+    from_path: str,
+    to_path: str,
+    bucket_name: str = 'exported_from_api'
+) -> None:
+    '''from_pathのデータをGCS上の指定したバケットのto_pathへ格納する
+
+    Args:
+        from_path (str): 転送元のデータのパス
+        to_path (str): 転送先のディレクトリのまでのパス
+        bucket_name (str, optional): バケット名. Defaults to 'exported_from_api'.
+    '''
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(to_path)
+
+    blob.upload_from_filename(from_path)
+
